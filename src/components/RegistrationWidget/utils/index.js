c20416aa2f487aa08c112c71cc6182b2108b5875
Jim Horn <jim@jdhorn.com>
2023-06-27 09:20:03 -0700

    Misc updates

--- src/components/RegistrationWidget/utils/index.js
+++ src/components/RegistrationWidget/utils/index.js
@@ -1,5 +1,5 @@
+export * from './createMap';
 export * from './getRandomFormValue';
 export * from './mockService';
 export * from './renderFormField';
 export * from './useFormState';
-export * from './createMap';
